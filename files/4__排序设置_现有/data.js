c110d21c359504a390fc0b967ff7839799ca9255
mykohuang <1018511036@qq.com>
2017-11-06 11:03:13 +0800

    1.0
    1.0

---
+++ files/4__排序设置_现有/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="4__排序设置_现有.html",d="generationDate",e=new Date(1509937126285.95),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="0e8d8e753f9e4901b8ab333351ee75fc",q="type",r="Axure:Page",s="name",t="4. 排序设置_现有",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="86aaeb5f1e2f4a74b60b7a62a38503da",Y="label",Z="friendlyType",ba="Image",bb="imageBox",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=3000,bi="height",bj=1971,bk="75a91ee5b9d042cfa01b8d565fe289c0",bl="imageOverrides",bm="images",bn="normal~",bo="images/4__排序设置_现有/u3.png",bp="masters",bq="objectPaths",br="86aaeb5f1e2f4a74b60b7a62a38503da",bs="scriptId",bt="u3";
+return _creator();
+})());
---
+++ files/5__进入常用应用/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="5__进入常用应用.html",d="generationDate",e=new Date(1509937126497.5),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="12562aec1ef342cda284ccf6600c09aa",q="type",r="Axure:Page",s="name",t="5. 进入常用应用",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="759a29a95149467a9d682ef7870d86a9",Y="label",Z="friendlyType",ba="Image",bb="imageBox",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=3000,bi="height",bj=1971,bk="75a91ee5b9d042cfa01b8d565fe289c0",bl="imageOverrides",bm="images",bn="normal~",bo="images/5__进入常用应用/u16.png",bp="masters",bq="objectPaths",br="759a29a95149467a9d682ef7870d86a9",bs="scriptId",bt="u16";
+return _creator();
+})());
